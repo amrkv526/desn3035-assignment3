85264ffa52d338a94a11b71d5cdb4640d723dcf3
amrkv526 <93342376+amrkv526@users.noreply.github.com>
2024-12-04 12:45:37 -0500

    new app setup

---
+++ desn3035-assignment3/components/Navbar.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/articles">Articles</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
+  );
+}
